19cde3de5b6a1a4bc01e6778ae034b308125ccea
Pedro Eugenio Ricciardi <riccip@hotmail.com>
2021-06-25 16:49:38 -0400

    v0.51

--- interface.js
+++ interface.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     let squares = document.querySelectorAll(".square");
-
     squares.forEach((square) => {
         square.addEventListener('click', handleClick)
     })
